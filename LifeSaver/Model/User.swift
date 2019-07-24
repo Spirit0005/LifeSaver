107ceceb8b9779acc3e769af59a8b78cf0025989
Hamza <m_hamzak1997@yahoo.com>
2019-07-24 23:57:20 +0500

    user class done

--- LifeSaver/Model/User.swift
+++ LifeSaver/Model/User.swift
@@ -9,5 +9,27 @@
 import Foundation
 import FirebaseAuth
 
+class user{
+    
+    var firstName:String
+    var lastName:String
+    var age:Int
+    var gender:String
+    var height:Int
+    var bloodGroup:String
+    var lastBD:Date
+    
+    
+    init(firstName:String,lastName:String,age:Int,gender:String,height:Int,bloodGroup:String,lastBD:Date) {
 
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.gender = gender
+        self.height = height
+        self.bloodGroup = bloodGroup
+        self.lastBD = lastBD
+    }
+    
+}
 
